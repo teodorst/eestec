630cf1d1e41ef73648f07478173e2c36791a2965
arotaru <nucleul.tocilarilor@gmail.com>
2016-11-12 00:47:25 +0200

    semibun

--- gesture.py
+++ gesture.py
@@ -10,12 +10,11 @@ while(cap.isOpened()):
     crop_img = img
     grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
     value = (35, 35)
-    
+
     blurred = cv2.GaussianBlur(grey, value, 0)
-    
-    _, thresh1 = cv2.threshold(blurred, 127, 255,
-                               cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    
+
+    _, thresh1 = cv2.threshold(blurred, 100, 255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
 
     cv2.imshow('Thresholded', thresh1)
 
